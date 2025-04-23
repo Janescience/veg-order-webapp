f876a99ee247055f7f46e609daebd2cb2604ec9b
janwit.si <janescience.top@gmail.com>
2025-04-24 01:17:23 +0700

    Test

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
-// 👇 เปลี่ยนลิงก์ด้านล่างให้เป็นของคุณ
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw3zWzBDyaPL9DJ6Tby2hS443_8azGsFVccwmpbWM-HFzK7XjD90pRp9HRIP6VsLtwB3A/exec";
+// PROD
+// const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw3zWzBDyaPL9DJ6Tby2hS443_8azGsFVccwmpbWM-HFzK7XjD90pRp9HRIP6VsLtwB3A/exec";
+
+// DEV
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby_gRKcUmNWIe5Jlv6o4JODG8SDocX4qM3UfxgoS1CgSlNpMhQCflLp7SHH1C4Vt9w/exec"
