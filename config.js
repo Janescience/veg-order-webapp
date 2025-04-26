319e76b078b3959748c16138a87ae2e7bc723052
janwit.si <janescience.top@gmail.com>
2025-04-26 14:58:04 +0700

    Config gas to dev

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // PROD
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw3zWzBDyaPL9DJ6Tby2hS443_8azGsFVccwmpbWM-HFzK7XjD90pRp9HRIP6VsLtwB3A/exec";
+// const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw3zWzBDyaPL9DJ6Tby2hS443_8azGsFVccwmpbWM-HFzK7XjD90pRp9HRIP6VsLtwB3A/exec";
 
 // DEV
-// const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby_gRKcUmNWIe5Jlv6o4JODG8SDocX4qM3UfxgoS1CgSlNpMhQCflLp7SHH1C4Vt9w/exec"
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby_gRKcUmNWIe5Jlv6o4JODG8SDocX4qM3UfxgoS1CgSlNpMhQCflLp7SHH1C4Vt9w/exec"
