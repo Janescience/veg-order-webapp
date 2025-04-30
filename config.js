b46875c542430bbbb9814ae5514f62a7d4dca561
janwit.si <janescience.top@gmail.com>
2025-04-30 22:46:15 +0700

    fix

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // PROD
 const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw3zWzBDyaPL9DJ6Tby2hS443_8azGsFVccwmpbWM-HFzK7XjD90pRp9HRIP6VsLtwB3A/exec";
-
 // DEV
 // const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby_gRKcUmNWIe5Jlv6o4JODG8SDocX4qM3UfxgoS1CgSlNpMhQCflLp7SHH1C4Vt9w/exec"
