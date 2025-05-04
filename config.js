5da9d0367430096f24d7b4c0261abc3c1c90c187
janwit.si <janescience.top@gmail.com>
2025-05-04 11:57:59 +0700

    Update get default data

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // PROD
-// const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw3zWzBDyaPL9DJ6Tby2hS443_8azGsFVccwmpbWM-HFzK7XjD90pRp9HRIP6VsLtwB3A/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw3zWzBDyaPL9DJ6Tby2hS443_8azGsFVccwmpbWM-HFzK7XjD90pRp9HRIP6VsLtwB3A/exec";
 // DEV
-
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw3Psnrrs_kFL_D8klO-opp_5gxdKs0jUqN3DU8hchGAGxatVw2qSjvlU8U_01j5YVp/exec"
+// const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw3Psnrrs_kFL_D8klO-opp_5gxdKs0jUqN3DU8hchGAGxatVw2qSjvlU8U_01j5YVp/exec"
